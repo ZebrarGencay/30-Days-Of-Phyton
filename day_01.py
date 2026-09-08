a96416d8bca09a2460536c5dc09112bcb954b4f3
ZebrarGencay <knvjphmk87@privaterelay.appleid.com>
2026-09-09 01:04:26 +0200

    Update day_01.py

--- day_01.py
+++ day_01.py
@@ -19,8 +19,8 @@ print(3 // 2) # Floor division operator(//)
 print(type(10))                  # Int
 print(type(3.14))                # Float
 print(type(1 + 3j))              # Comple
-print(type('Asabeneh'))          # String
+print(type('Ebrar'))          # String
 print(type([1, 2, 3]))           # List
-print(type({'name': 'Asabeneh'})) # Dictionary
+print(type({'name': 'Ebrar'})) # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Tuple
 
