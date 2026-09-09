8e4588047e14e2f19cd43fb2fb60e93b4c1aa128
ZebrarGencay <knvjphmk87@privaterelay.appleid.com>
2026-09-09 12:34:42 +0200

    Add files via upload

---
+++ day_02.py
@@ -0,0 +1,41 @@
+
+# Variables in Pyhton
+
+first_name = 'Zeynep'
+last_name = 'Gencay'
+country = 'Poland'
+city = 'Warsaw'
+age = 20
+is_married = False
+skills = ['Pyhton']
+person_info = {
+    'firstname': 'Ebrar'
+    'lastname' 'Gencay'
+    'country' 'Poland'
+    'city' 'Warsaw'
+}
+
+#Printing the values stored in the variables
+
+print('First name: ', first_name)
+print('First name length: ', len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('Country: ', country)
+print('City: ', city)
+print('Age: ', age)
+print('Married: ', is_married)
+print('Skills: ', skills)
+print('Person information: ', person_info)
+
+# Declaring multiple variables in one line
+
+first_name, last_name, country, age, is_married = 'Ebrar', 'Gencay', 'Poland', 20, False
+
+print(first_name, last_name, country, age, is_married)
+print('First name: ', first_name)
+print('Last name: ', last_name)
+print('Country: ', country)
+print('Age: ', age)
+print('Married: ', is_married)
+      
